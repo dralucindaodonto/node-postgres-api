104a27e6d05b8b5b8a6952d5cdbaef363a2bb45d
dralucindaodonto <dralucindaodonto@gmail.com>
2022-05-18 05:57:04 -0300

    save

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const app = require('./src/app');
+const app = require("./src/app");
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Aplicação rodando na porta ${port}`);
+  console.log(`Aplicação rodando na porta ${port}`);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,36 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/api', ((req, res) => {
-    res.status(200).send({
-        success: true,
-        message: 'Tudo funcionando',
-        version: '1.0.0',
-    });
-}));
+router.get("/api", (req, res) => {
+  const signer = require("node-signpdf").default;
+  const fs = require("fs");
+  const { plainAddPlaceholder } = require("node-signpdf/dist/helpers");
+
+  const pdfSignedPath = `./signed.pdf`;
+  const pdfBuffer = fs.readFileSync(`./source.pdf`);
+  const certBuffer = fs.readFileSync(`./certificate.p12`);
+
+  let inputBuffer = plainAddPlaceholder({
+    pdfBuffer,
+    reason: "Signed Certificate.",
+    contactInfo: "sign@example.com",
+    name: "Example",
+    location: "Jakarta",
+    signatureLength: certBuffer.length,
+  });
+
+  const signedPdf = signer.sign(inputBuffer, certBuffer, {
+    asn1StrictParsing: true,
+  });
+
+  fs.writeFileSync(pdfSignedPath, signedPdf);
+
+  res.status(200).send({
+    success: true,
+    message: "Tudo funcionando",
+    version: "1.0.0",
+  });
+});
 
 module.exports = router;
